df3c2d4afea506a13c916ce9741473e9a219d62a
Noah Thederahn <thederahnn16@hanover.edu>
2016-02-09 19:41:43 -0500

    Close #43

--- assignment4tests.ml
+++ assignment4tests.ml
@@ -16,6 +16,8 @@ let t3a = try (try (thunk_of_eval ((fun x -> raise (Failure "")), 4))
 let t3b = thunk_of_eval ((fun x -> x + 1), 5) () = 6
 
 let t4a = try_thunk (fun () -> raise (Failure "hi")) = None
+let t4b = try_thunk (fun () -> 5) = Some 5
+let t4c = try_thunk (fun () -> "Bacon") = Some "Bacon"
 
 let t5a = let f = fun () -> raise (Failure "")
           in try (try (thunk_of_pair (f, f)) with Failure "" -> (fun () -> (1, 1))) () =
