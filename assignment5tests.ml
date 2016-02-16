1cd3fbc2a152bc707a56a8d62e98424f3e4d99f8
Noah Thederahn <thederahnn16@hanover.edu>
2016-02-16 16:47:52 -0500

    wrote tests for power (Close #89)

--- assignment5tests.ml
+++ assignment5tests.ml
@@ -30,7 +30,10 @@ let t12c = subst (Add (Var, Int 1), Sub (Int 2, Var)) =
 let t12d = subst (Add (Var, Int 1), Var) = Add (Var, Int 1)
 let t12e = subst (Add (Var, Int 1), Int 42) = Int 42
 
-let t13a = power 3 = Mul (Mul (Var, Var), Var)
+let t13a = power (Var, 3) = Mul (Mul (Var, Var), Var)
+let t13b = power (Var, 0) = Int 1
+let t13c = power (Var, 1) = Var
+let t13d = power (Var, 6) = Mul (Mul (Mul (Mul (Mul (Var, Var), Var), Var), Var), Var)
 
 let t14a = term (2, 1) = Mul(Int 2, Var)
 
